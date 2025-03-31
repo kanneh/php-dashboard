998695f4e69e17376d4145067e5b692c57bc6605
Alhaji Kanneh <alhajikanneh@gmail.com>
2025-03-31 19:31:31 +0000

    left join missing left value

--- src/Utils/PSSLMerge.php
+++ src/Utils/PSSLMerge.php
@@ -171,7 +171,7 @@ class PSSLMerge{
                     foreach ($row2 as $key => $value) {
                         $row2[$key] = null;
                     }
-                    $this->data[] = $this->buildRow($row2,$row2,$src2name);
+                    $this->data[] = $this->buildRow($row,$row2,$src2name);
                 }
             }
             // $this->data = array_map(function($row) use ($data2,$joinConditions,$src2name){
